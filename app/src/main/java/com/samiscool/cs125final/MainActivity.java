2d2159b1a89eb6a39b16ccfe4addc2c80b498fc4
BuildTools <unconfigured@null.spigotmc.org>
2018-04-27 13:58:21 -0500

    Wrote translate function and started adding GSON implimentation

--- app/src/main/java/com/samiscool/cs125final/MainActivity.java
+++ app/src/main/java/com/samiscool/cs125final/MainActivity.java
@@ -13,37 +13,64 @@ import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
+    /** This is the TAG name for this class. Used in Log messages. */
     private static final String TAG = "MainActivity";
 
-    public static String JSON = "";
-
+    /** Code that runs automatically whenever the app comes into the foreground.
+     * This does not need to be modified.
+     * @param savedInstanceState a saved state from when the app left the foreground.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        callAPI("Imyoda.");
-        Log.d(TAG, "onCreate: JSON =" + JSON);
     }
 
+    /**
+     * This function sends user input data to the API and then calls the JSON Parser.
+     * This function should be called when the 'Translate' button is pressed.
+     * <strong> This code should not be modified. </strong>
+     * @param toTranslate Text from user input.
+     */
     public void callAPI(final String toTranslate) {
-        Log.d(TAG, "callAPI: i got called");
+        Log.d(TAG, "callAPI: I was activated");
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="http://api.funtranslations.com/translate/yoda.json?text=" + toTranslate;
+        Log.d(TAG, "callAPI: using url: " + url);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.d(TAG,"response!");
-                        JSON = response;
+                        Log.d(TAG, "onResponse: Received Response!");
+                        Log.d(TAG, "onResponse: " + response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, error.toString());
-                Log.d(TAG, "Here is the ERROR!");
-                JSON = "Volley request done goofed.";
+                Log.e(TAG, "onErrorResponse: Error returning data or request limit exceeded.");
             }
         });
         queue.add(stringRequest);
+    }
+
+    /**
+     * This function gets the translation from the JSON object and sets the text view.
+     * @param json json string from callAPI
+     */
+    public void translate(final String json) {
+        String translation = getObj(json).get("contents").getAsJsonObject().get("translation").getAsString();
+        //Set textView = translation
+    }
+
+    /**
+     * This is a helper function for translate.
+     * Takes a string and returns a JSON object.
+     * This code does not need to be modified.
+     * @param json JSON response from API.
+     * @return JSON object built from API response.
+     */
+    private static JsonObject getObj(final String json) {
+        JsonParser parser = new JsonParser();
+        return parser.parse(json).getAsJsonObject();
     }
 }
